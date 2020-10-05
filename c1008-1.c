03ad8d6670312391b1a82664e6763e80b0a920cc
Yen Hsu <yenhsu@Yens-MacBook-Pro.local>
2020-10-05 13:45:26 +0800

    all complete

--- c1008-1.c
+++ c1008-1.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-int main(){
-    int hourA,minA,secA,hourB,minB,secB,hourOut,minOut,secOut;
-    scanf("%d:%d:%d",&hourA,&minA,&secA);
-    scanf("%d:%d:%d",&hourB,&minB,&secB);
-    secOut=secA+secB;
-    if (secOut>=60)
+int main()
+{
+    int hourA, minA, secA, hourB, minB, secB, hourOut, minOut, secOut;
+    scanf("%d:%d:%d", &hourA, &minA, &secA);
+    scanf("%d:%d:%d", &hourB, &minB, &secB);
+    secOut = secA + secB;
+    if (secOut >= 60)
     {
-        minOut=secOut/60;
-        secOut%=60;
+        minOut = secOut / 60;
+        secOut %= 60;
     }
-    minOut=minA+minB;
-    if (minOut>=60)
+    minOut = minA + minB;
+    if (minOut >= 60)
     {
-        hourOut=minOut/60;
-        minOut%=60;
+        hourOut = minOut / 60;
+        minOut %= 60;
     }
-    hourOut=hourA+hourB;
-    hourOut%=24;
-    printf("%.2d:%.2d:%.2d\n",hourOut,minOut,secOut);
+    hourOut = hourA + hourB;
+    hourOut %= 24;
+    printf("%.2d:%.2d:%.2d\n", hourOut, minOut, secOut);
     return 0;
 }
---
+++ c1008-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i < 12; i++)
+    {
+        printf("%s", "NCUE");
+        printf("%d\n", i * 10 + 1);
+    }
+    return 0;
+}
