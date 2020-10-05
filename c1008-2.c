a049dd3c111c3b00d91871575ea81bb1b33f4b9c
Yen Hsu <yenhsu@Yens-MacBook-Pro.local>
2020-10-05 13:40:09 +0800

    c1008-2.c completed!

---
+++ c1008-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    scanf("(%d,%d)", &x, &y);
+    if (x == 0 || y == 0)
+    {
+        if (x == 0 && y == 0)
+            printf("%s", "在原點\n");
+        else if (x == 0)
+            printf("%s", "在y軸上\n");
+        else
+            printf("%s", "在x軸上\n");
+    }
+    else if (x > 0)
+    {
+        if (y > 0)
+            printf("%s", "在第一象限\n");
+        else
+            printf("%s", "在第四象限\n");
+    }
+    else
+    {
+        if (y > 0)
+            printf("%s", "在第二象限\n");
+        else
+            printf("%s", "在第三象限\n");
+    }
+}
